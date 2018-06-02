1bd62223207d14276e29ddac7ef08e50540b11a7
faithwanjiku07 <irungufaith974@gmail.com>
2018-06-02 09:50:54 +0300

    First commit

---
+++ trial.py
@@ -0,0 +1,19 @@
+list1=[1,2,3,4,5]
+print(list1)
+
+list2=['faith', "martha", "loryne",]
+print(list2)
+
+list3=["faith", "martha", "loryne", 100,200]
+print(list3)
+
+tuple=('nairobi', 'kisumu', 254,200)
+print(tuple)
+
+first_list=[1,2,'faith', "martha", "loryne", 1,2,3]
+first_set=set(first_list)
+print(first_set)
+
+
+dict={"Name":"Faith", "Age":21,"Nationality":"Kenya","Address":"Nairobi"}
+print(dict)
